80f8419a0321a0a444482e9d52c27e27e93ffb71
Bjorn <11698329+bjorn1213@users.noreply.github.com>
2023-10-01 14:37:45 +0200

    Add total price to cart

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,9 +2,18 @@ import CartProductCard from "../components/CartProductCard";
 import { getCart, setProductAmount } from "../utility/cartUtility";
 import styles from "./CartPage.module.css";
 import { useState } from "react";
+import priceFormatter from "../utility/priceFormatter";
 
 function CartPage() {
   const [cartObject, setCart] = useState(getCart());
+  const productCount = Object.keys(cartObject).length;
+
+  let total = 0;
+
+  for (let productID in cartObject) {
+    const product = cartObject[productID];
+    total += product.quantity * product.price;
+  }
 
   function setCartProductAmount(product) {
     return (amount) => {
@@ -15,17 +24,22 @@ function CartPage() {
 
   return (
     <>
-      <div className={styles.cartItemContainer}>
-        {Object.entries(cartObject).map((cartItem) => {
-          return (
-            <CartProductCard
-              key={cartItem[0]}
-              product={cartItem[1]}
-              amountSetter={setCartProductAmount(cartItem[1])}
-            />
-          );
-        })}
-      </div>
+      {productCount ? (
+        <div className={styles.cartItemContainer}>
+          {Object.entries(cartObject).map((cartItem) => {
+            return (
+              <CartProductCard
+                key={cartItem[0]}
+                product={cartItem[1]}
+                amountSetter={setCartProductAmount(cartItem[1])}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <div className={styles.cartItemContainer}>No items in cart</div>
+      )}
+      <div className={styles.cartTotalContainer}>€{priceFormatter(total)}</div>
     </>
   );
 }
